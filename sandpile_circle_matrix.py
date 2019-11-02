18dfd84760a8add44f03c0b3c77139180ca9f28b
Persjanow Jakub <jakubpersjanow@gmail.com>
2019-11-02 18:42:53 +0100

    added plotting data for circle plate, no movement

--- sandpile_circle_matrix.py
+++ sandpile_circle_matrix.py
@@ -2,7 +2,8 @@ from random import randint
 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import colors as c
+from collections import Counter
+from tabulate import tabulate as tab
 
 from matplotlib import animation
 
@@ -12,7 +13,7 @@ class Sandpile:
     Class representing sandpile model for Self-Organized Criticality
 
     :params
-    numer_radial = number representing r values
+    numer_radial = number representing r values (must be odd number)
     number_angles = numer representing fi values
     array = array representing sandpile with r and fi
     max_peak = maximum sand grains on position [r][fi]
@@ -26,21 +27,33 @@ class Sandpile:
         self.max_peak = max_peak
         self.topple_count = 0
         self.mass_count = 0
+        self.mass_fallen_count = 0
+        self.mass_left_count = 0
+        self.mass_when_iteration = []
+        self.when_topple = []
 
-        self.angles_array = np.linspace(0, 2 * np.pi, number_angles)
+        self.number_of_simulations = 0
+
+        self.angles_array = np.linspace(0, 2 * np.pi, number_angles + 1)
         self.radial_array = np.linspace(0, number_radial, number_radial + 1)
 
     def add_grain(self, radial_position):
         self.mass_count += 1
         self.array[radial_position][randint(0, self.number_angles - 1)] += 1
 
-    def topple(self, radial_position, angle_position):
+    def topple(self, radial_position, angle_position, iteration):
+
+        #gather data
         self.topple_count += 1
+        self.when_topple.append(iteration)
+        #execute topple
         self.array[radial_position][angle_position] -= 3
 
         #one grain topples downwards
         if radial_position < self.number_radial - 1:
             self.array[radial_position + 1][angle_position] += 1
+        else:
+            self.mass_fallen_count += 1
 
         #one grain topples LEFT
 
@@ -56,42 +69,74 @@ class Sandpile:
         else:
             self.array[radial_position][angle_position + 1] += 1
 
-    def check_pile(self):
+    def check_pile(self, iteration):
         for r in range(0, self.number_radial, 1):
-            for fi in range(0, self.number_angles, 1):
+            for theta in range(0, self.number_angles, 1):
 
-                if self.array[r][fi] < self.max_peak:
-                    self.array[r][fi] = self.array[r][fi]
+                if self.array[r][theta] < self.max_peak:
+                    self.array[r][theta] = self.array[r][theta]
 
                 else:
-                    self.topple(r, fi)
+                    self.topple(r, theta, iteration)
 
     def simulate(self, number_of_simulations):
+        self.number_of_simulations = number_of_simulations
 
-        for i in range(0, number_of_simulations, 1):
+        for iteration_num in range(0, number_of_simulations, 1):
             self.add_grain(0)
-            self.check_pile()
+            self.check_pile(iteration_num)
+            self.mass_when_iteration.append(self.mass_count - self.mass_fallen_count)
             print(self.array)
 
-    def update(self):
+    def count_mass_left(self):
+        self.mass_left_count = int(np.sum(self.array))
 
-        self.add_grain(0)
-        self.check_pile()
+    def plot(self, type='sandpile'):
 
-    def plot(self):
-        # A, B = np.meshgrid(self.angles_array, self.radial_array)
+        #plot sandpile after simulation
+        if type == 'sandpile':
+            fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
+            cb = ax.pcolormesh(self.angles_array, self.radial_array, self.array, edgecolors='k', linewidths=1)
+            ax.set_yticks([])
+            ax.set_theta_zero_location('N')
+            ax.set_theta_direction(-1)
+            plt.colorbar(cb, orientation='vertical')
+            plt.show()
 
-        fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
-        cb = ax.pcolormesh(self.angles_array, self.radial_array, self.array, edgecolors='k', linewidths=1)
-        ax.set_yticks([])
-        ax.set_theta_zero_location("N")
-        ax.set_theta_direction(-1)
-        plt.colorbar(cb, orientation='vertical')
-        plt.show()
+        #plot iteration / mass of left pile on plate
+        if type == 'mass':
+            simulation_array = []
+            for i in range(0, self.number_of_simulations, 1):
+                simulation_array.append(i)
+
+            plt.plot(simulation_array, self.mass_when_iteration)
+            plt.title("Left Mass on pile during iterations")
+            plt.xlabel("Iteration")
+            plt.ylabel("Mass of Pile on plate")
+            plt.show()
+
+
+        #plot iteration / topple
+        if type == 'topple':
+            lists = sorted(Counter(self.when_topple).items())
+            when_topple, topple_count = zip(*lists)
+
+            plt.bar(when_topple, topple_count)
+            plt.xlabel('Iteration Number')
+            plt.ylabel('Topple Count')
+            plt.show()
+
+    def analyze_data(self):
+        data = {"Topple Count": self.topple_count, "Fallen mass": self.mass_fallen_count}
+        print(data)
+
+        self.plot()
+        self.plot(type='mass')
+        self.plot(type='topple')
+
 
 
 SANDPILE = Sandpile(5, 36, 5)
 SANDPILE.simulate(1000)
-SANDPILE.plot()
-SANDPILE.simulate(100)
-SANDPILE.plot()
+SANDPILE.analyze_data()
+
